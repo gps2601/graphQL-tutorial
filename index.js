9727fef75bffc5c18d66795dc3960346b4781664
Geoffrey Smith <geoffreysmith@geoffreys-mbp.home>
2019-11-03 21:23:25 +0000

    Adding the ability to query weather there is a phone number or not

--- index.js
+++ index.js
@@ -37,9 +37,14 @@ const typeDefs = gql`
         id:ID!
     }
 
+    enum YesNo {
+        YES
+        NO
+    }
+
     type Query {
         personCount: Int!
-        allPersons: [Person!]!
+        allPersons(phone: YesNo): [Person!]!
         findPerson(name: String!): Person
     }
 
@@ -56,7 +61,14 @@ const typeDefs = gql`
 const resolvers = {
     Query: {
         personCount: () => persons.length,
-        allPersons: () => persons,
+        allPersons: (root, args) => {
+            if(!args.phone) {
+                return persons
+            }
+            const byPhone = (person) => 
+                args.phone === 'YES' ? person.phone : !person.phone
+            return persons.filter(byPhone)
+        },
         findPerson: (root, args) => 
         persons.find(p => p.name === args.name)
     },
@@ -75,7 +87,7 @@ const resolvers = {
                     invalidArgs: args.name,
                 })
             }
-            
+
             const person = {...args, id: uuid()}
             persons = persons.concat(person)
             return person
